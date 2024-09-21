af18881e458d56646c350b9cd074e3a33cc2baa4
corey <corey.cushnie@gmail.com>
2024-09-21 07:57:10 -0400

    entry page

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl text-primary font-bold">Gaze App</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile">Profile-er</Link>
+    </View>
+  );
+}
