0055871857888aba8659a4dba2d22847e54ab401
corey <corey.cushnie@gmail.com>
2024-09-22 09:03:14 -0400

    page build

---
+++ app/(tabs)/alerts.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Alerts = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Alerts</Text>
+    </View>
+  );
+};
+
+export default Alerts;
