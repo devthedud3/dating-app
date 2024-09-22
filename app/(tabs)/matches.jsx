a2b80f7f1783e9f0c7008842c6585b7dc0740d75
corey <corey.cushnie@gmail.com>
2024-09-22 09:03:27 -0400

    page build

---
+++ app/(tabs)/matches.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Match = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Match</Text>
+    </View>
+  );
+};
+
+export default Match;
