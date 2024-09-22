84a55ddc270b3953d70644ade2e4fa2b60804551
corey <corey.cushnie@gmail.com>
2024-09-22 09:02:59 -0400

    layout build

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Tabs } from "expo-router";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#FD6363",
+        }}
+      >
+        <Tabs.Screen
+          name="feed"
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <FontAwesome6 name="connectdevelop" size={20} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="matches"
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <FontAwesome6 name="wifi" size={20} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="messages"
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <AntDesign name="message1" size={20} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="alerts"
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <FontAwesome6 name="bell" size={20} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  );
+};
+
+export default TabsLayout;
