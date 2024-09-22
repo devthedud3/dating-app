186954cb3e1c52443cdf124946bb2d6e5dc6bfe0
corey <corey.cushnie@gmail.com>
2024-09-22 09:02:15 -0400

    layout build

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
 
 const RootLayout = () => {
   SplashScreen.preventAutoHideAsync();
-  const [fonts, error] = useFonts({});
+  const [loaded, error] = useFonts({
+    pblack: require("../assets/fonts/Poppins-Black.ttf"),
+    pblacki: require("../assets/fonts/Poppins-BlackItalic.ttf"),
+    pbold: require("../assets/fonts/Poppins-Bold.ttf"),
+    pboldi: require("../assets/fonts/Poppins-BoldItalic.ttf"),
+    pxbold: require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    pxboldi: require("../assets/fonts/Poppins-ExtraBoldItalic.ttf"),
+    pregular: require("../assets/fonts/Poppins-Regular.ttf"),
+    psemiBold: require("../assets/fonts/Poppins-SemiBold.ttf"),
+    plight: require("../assets/fonts/Poppins-Light.ttf"),
+    pthin: require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
