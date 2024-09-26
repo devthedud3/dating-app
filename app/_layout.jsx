26327dc505a53ec2888f7e7fa5fcd7bad12e319a
corey <corey.cushnie@gmail.com>
2024-09-26 09:38:58 -0400

    update layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -33,7 +33,7 @@ const RootLayout = () => {
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: true }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
     </>
