a9dfea9397859d3f51691cc9a26011c12aa12f26
corey <corey.cushnie@gmail.com>
2024-09-26 09:39:33 -0400

    build feed

--- app/(tabs)/feed.jsx
+++ app/(tabs)/feed.jsx
@@ -1,9 +1,11 @@
 import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
+import Back from "../../components/Back";
 
 const Feed = () => {
   return (
     <SafeAreaView className="flex-1 items-center">
+      <Back />
       <View className="w-full p-4">
         <Text className="text-xs font-pbold text-gray-500">EVENTS</Text>
       </View>
