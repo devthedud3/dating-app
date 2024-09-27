7697aad8da196f45a3fcb2a6af1c598849d7e32a
corey <corey.cushnie@gmail.com>
2024-09-26 21:28:34 -0400

    add signout to alerts

--- app/(tabs)/alerts.jsx
+++ app/(tabs)/alerts.jsx
@@ -1,7 +1,17 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+import { useGlobalContext } from "../../context/GlobalContext";
+import { Redirect } from "expo-router";
 
 const Alerts = () => {
+  const { isLoggedIn, setIsLoggedIn, setUser } = useGlobalContext();
+  useEffect(() => {
+    setIsLoggedIn(false);
+    setUser(null);
+  }, []);
+
+  if (!isLoggedIn) return <Redirect href="/" />;
+
   return (
     <View className="flex-1 items-center justify-center">
       <Text>Alerts</Text>
