7a8d90011d58bd24e00a98be2b26ee8263acd507
corey <corey.cushnie@gmail.com>
2024-09-26 21:28:43 -0400

    Global context

---
+++ context/GlobalContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { currentUser } from "../lib/authFunctions";
+
+const GlobalContext = createContext();
+export const useGlobalContext = () => useContext(GlobalContext);
+
+export const GlobalProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        isLoggedIn,
+        setIsLoggedIn,
+        user,
+        setUser,
+        isLoading,
+        setIsLoading,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
