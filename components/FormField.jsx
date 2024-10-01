0c38797c3641fce6ab1f1f99bae0628f444a318f
corey <corey.cushnie@gmail.com>
2024-09-30 22:45:21 -0400

    form build

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -3,20 +3,29 @@ import React, { useState } from "react";
 import Feather from "@expo/vector-icons/Feather";
 import { TouchableOpacity } from "react-native";
 
-const FormField = ({ name, value, onChange, keyboardType, formStyles }) => {
+const FormField = ({
+  name,
+  value,
+  onChange,
+  keyboardType,
+  formStyles,
+  placeholder,
+  secureTextEntry,
+}) => {
   const [isVisible, setIsVisible] = useState(false);
 
   return (
-    <View className={`space-y-2 ${formStyles}`}>
+    <View className={`${formStyles}`}>
       <Text className="text-sm font-pregular">{name}</Text>
-      <View className="w-full h-12 px-4 flex flex-row items-center bg-gray-200 rounded-lg border-2 border-gray-300  focus:border-primary">
+      <View className="w-full h-12 px-4 flex flex-row items-center mt-2 bg-slate-100 rounded-sm border border-gray-300  focus:border-primary">
         <TextInput
           className="flex-1 font-psemibolds"
           value={value}
           onChangeText={onChange}
           keyboardType={keyboardType}
           autoCapitalize={false}
-          secureTextEntry={keyboardType === "password" && !isVisible}
+          placeholder={placeholder}
+          secureTextEntry={secureTextEntry && !isVisible}
         />
         {keyboardType === "password" && (
           <TouchableOpacity onPress={() => setIsVisible(!isVisible)}>
