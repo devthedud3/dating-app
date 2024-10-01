cdfc2af85483d4d4e7bf54678bf68d3cf9c509f1
corey <corey.cushnie@gmail.com>
2024-09-30 22:45:06 -0400

    back button

--- components/Back.jsx
+++ components/Back.jsx
@@ -1,20 +1,18 @@
-import { View, TouchableOpacity } from "react-native";
+import { Pressable } from "react-native";
 import React from "react";
 import { router } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 
 const Back = () => {
   return (
-    <View className="w-full px-4 h-16 justify-center">
-      <TouchableOpacity
-        className="w-8 h-8 justify-center items-center  rounded-full"
-        onPress={() => {
-          router.back();
-        }}
-      >
-        <Ionicons name="arrow-back" size={24} color="black" />
-      </TouchableOpacity>
-    </View>
+    <Pressable
+      className="w-8 h-8 justify-center items-center  rounded-full"
+      onPress={() => {
+        router.back();
+      }}
+    >
+      <Ionicons name="arrow-back" size={24} color="black" />
+    </Pressable>
   );
 };
 
