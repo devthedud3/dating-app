e9a44b33975c2a2637fb0eff539aaa9cf89368ca
corey <corey.cushnie@gmail.com>
2024-09-30 22:44:58 -0400

    build feed

--- app/(tabs)/feed.jsx
+++ app/(tabs)/feed.jsx
@@ -1,15 +1,30 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, StatusBar, Pressable } from "react-native";
+import React, { useState } from "react";
 import Back from "../../components/Back";
+import OnboardingModal from "../../components/modals/onboarding";
 
 const Feed = () => {
+  const [showModal, setShowModal] = useState(true);
+  if (showModal)
+    return (
+      <SafeAreaView className="absolute h-full bg-white">
+        <OnboardingModal isVisible={showModal} setIsVisible={setShowModal} />
+        {/* <StatusBar hidden /> */}
+      </SafeAreaView>
+    );
   return (
-    <SafeAreaView className="flex-1 items-center">
-      <Back />
-      <View className="w-full p-4">
-        <Text className="text-xs font-pbold text-gray-500">EVENTS</Text>
-      </View>
-    </SafeAreaView>
+    <>
+      <SafeAreaView className="flex-1 items-center bg-white">
+        <View className="p-4 w-full">
+          <View className="flex w-full justify-between flex-row py-2">
+            <Back />
+          </View>
+          <View className="w-full ">
+            <Text className="text-xs font-pbold text-gray-500">EVENTS</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </>
   );
 };
 
